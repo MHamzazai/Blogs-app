c4ad9f003c1630f25e845aa71ecf4d5e0878459e
MHamzazai <farhatifraq@gmail.com>
2024-12-24 01:54:12 +0500

    first-commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import 'remixicon/fonts/remixicon.css'
 import Image from "next/image";
-import logo from "../../app/public/photos/logo.jpg";
 import Link from "next/link";
 
 export default function Header() {
@@ -10,6 +9,10 @@ export default function Header() {
     const toggleNavbar = (): void => {
         setisClick(!isClick);
     }
+    const closeMenu = () => {
+        setisClick(false);
+    }
+
     return (
         <main>
             <nav className="bg-black border-x-2 shadow-slate-950 shadow-lg">
@@ -17,7 +20,7 @@ export default function Header() {
                     <div className="flex items-center justify-between h-16">
                         <div className="flex items-center">
                             <div className="flex-shrink-0">
-                                <Image src={logo} alt="Logo" width={70} height={100} className="" />
+                                <Image src={"/photos/logo.jpg"} alt="Logo" width={70} height={100} className="" />
                             </div>
 
                         </div>
@@ -47,6 +50,12 @@ export default function Header() {
                               md:hover:bg-black md:hover:text-white md:text-xl lg:hover:text-2xl lg:hover:font-thin">
                                     Home
                                 </Link>
+                                <Link href={"/Blogs"} className="text-white hover:bg-white
+                                 hover:text-black transition-all rounded-sm
+                                md:rounded-none md:hover:scale-110 md:hover:underline
+                              md:hover:bg-black md:hover:text-white md:text-xl lg:hover:text-2xl lg:hover:font-thin">
+                                    Blogs
+                                </Link>
                             </div>
                         </div>
                         <div className="md:hidden flex items-center">
@@ -62,21 +71,35 @@ export default function Header() {
                 {isClick && (
                     <div className="md:hidden">
                         <div className="px-2 pt-2 pb-3 space-y-3 sm:px-4 flex flex-col text-center bg-slate-950">
-                            <Link href="https://github.com/mhamzazai/" target="_blank" className="text-white hover:bg-white hover:text-black
-                               rounded-sm hover:scale-95 transition-all text-xl font-semibold font-sans border bg-black">
+                            <Link href="https://github.com/mhamzazai/" target="_blank"
+                            className="text-white hover:bg-white hover:text-black rounded-sm
+                            hover:scale-95 transition-all text-xl font-semibold font-sans border
+                            bg-black" onClick={closeMenu}>
                                 Go To GitHub
                             </Link>
-                            <Link href="https://www.npmjs.com/~mhamzazai" target="_blank" className="text-white hover:bg-white hover:text-black
-                                rounded-sm text-xl hover:scale-95 transition-all font-semibold font-sans border bg-black">
+                            <Link href="https://www.npmjs.com/~mhamzazai" target="_blank"
+                            className="text-white hover:bg-white hover:text-black rounded-sm text-xl
+                            hover:scale-95 transition-all font-semibold font-sans border bg-black"
+                            onClick={closeMenu}>
                                 Go To Npm
                             </Link>
-                            <Link href="/About" className="text-white hover:bg-white hover:text-black
-                                rounded-sm text-xl hover:scale-95 transition-all font-semibold font-sans border bg-black">
+                            <Link href="/About" 
+                            className="text-white hover:bg-white hover:text-black rounded-sm text-xl
+                            hover:scale-95 transition-all font-semibold font-sans border bg-black"
+                            onClick={closeMenu}>
                                 About MySelf
                             </Link>
-                            <Link href="/" className="text-white hover:bg-white hover:text-black
-                                rounded-sm text-xl hover:scale-95 transition-all font-semibold font-sans border bg-black">
+                            <Link href="/" 
+                            className="text-white hover:bg-white hover:text-black rounded-sm text-xl
+                            hover:scale-95 transition-all font-semibold font-sans border bg-black"
+                            onClick={closeMenu}>
                                 Home
+                            </Link>
+                            <Link href="/Blogs" 
+                            className="text-white hover:bg-white hover:text-black rounded-sm text-xl
+                            hover:scale-95 transition-all font-semibold font-sans border bg-black"
+                            onClick={closeMenu}>
+                                Blogs
                             </Link>
                         </div>
                     </div>
--- src/components/types/CardType.ts
+++ src/components/types/CardType.ts
@@ -1,10 +1,24 @@
-import { StaticImageData } from "next/image";
-
 type cardType = {
-    blogno: number;
-    topicName: string;
-    imageType: StaticImageData;
-    description: string;
-    link: string;
-}
+  blogno: number;
+  imageType: string;
+  description: string;
+  blogName: string;
+  slug: string;
+};
 export default cardType;
+
+type blogsCardDetailsTypes = {
+  blogName: string;
+  firstHeading: string;
+  firstPara: string;
+  secondHeading: string;
+  secondPara: string;
+  thirdHeading: string;
+  thirdPara: string;
+  fourthHeading: string;
+  fourthPara: string;
+  fifthHeading: string;
+  fifthPara: string;
+  image: string;
+};
+export type { blogsCardDetailsTypes };
